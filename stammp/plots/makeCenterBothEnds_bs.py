5949b5a617d189cfae75be9c7b35fb10c6772c15
Christian Roth <christian.roth@mpibpc.mpg.de>
2016-09-04 15:11:01 +0200

    initial support of a bootstrapped center plot

---
+++ stammp/plots/makeCenterBothEnds_bs.py
@@ -0,0 +1,167 @@
+import argparse
+import os
+
+import numpy as np
+import pandas as pd
+
+from stammp.obj import gff, parclipsites
+from stammp.utils import execute
+from stammp.utils.argparse_helper import file_r, dir_rwx
+
+
+def create_parser():
+    parser = argparse.ArgumentParser(
+        description=(
+            'Plot PAR-CLIP data in sense and anti-sense direction around start '
+            'and stop positions given in the GFF file. The output filename is constructed '
+            'from parameters: outputdir/prefix+u+g+d+min+max+labelCenterA+labelBody+'
+            'labelCenterB+plotSmooth.pdf\n\n'
+            'Blue color represents PAR-CLIP signal on the sense strand, green color '
+            'represents PAR-CLIP signal on the anti-sense strand.'
+        )
+    )
+    parser.add_argument('pc_table', help='path to the PAR-CLIP *.table', type=file_r)
+    parser.add_argument('outputdir', help='output directory', type=dir_rwx)
+    parser.add_argument('prefix', help='prefix of filenames')
+    parser.add_argument('gff_file', help='GFF file used for plotting', type=file_r)
+    parser.add_argument('--downstream_bp', '-d', help='set downstream range',
+                        default=1000, type=int)
+    parser.add_argument('--upstream_bp', '-u', help='set upstream range',
+                        default=1000, type=int)
+    parser.add_argument('--gene_bp', '-g', help='set gene range',
+                        default=750, type=int)
+    parser.add_argument('--min_ts_len', help='minimum transcript size',
+                        default=0, type=int)
+    parser.add_argument('--max_ts_len', help='maximum transcript size',
+                        default=5000, type=int)
+    smooth_help = 'window size used for running mean smoothing'
+    parser.add_argument('--smooth_window', help=smooth_help, default=41, type=int)
+    label_cenA_help = 'plot label for the first center position'
+    parser.add_argument('--labelCenterA', help=label_cenA_help, default='TSS')
+    parser.add_argument('--labelBody', help='for body (between A and B)',
+                        default='gene')
+    label_cenB_help = 'plot label for the second center position'
+    parser.add_argument('--labelCenterB', help=label_cenB_help, default='pA')
+    parser.add_argument('--title', help='plot title')
+    parser.add_argument('--cleanup', action='store_true',
+                        help='remove temporary files')
+    parser.add_argument('--seed', type=int, default=42)
+    parser.add_argument('--n_bs_iterations', default=500, type=int)
+    return parser
+
+
+def main():
+    parser = create_parser()
+    args = parser.parse_args()
+
+    np.random.seed(args.seed)
+
+    anno = gff.GFF(args.gff_file)
+    anno.filterSize(args.min_ts_len, args.max_ts_len)
+    pc = parclipsites.ParclipSites()
+    pc.loadFromFile(args.pc_table)
+
+    cut_len = args.downstream_bp + args.upstream_bp + 2 * args.gene_bp + 2
+
+    def aggregate_data(g, sense=True):
+        if anno.strand[g] == '+':
+            values_upstream = pc.getValues(anno.chr[g], anno.start[g], anno.strand[g],
+                                           sense, args.upstream_bp, args.gene_bp)
+            values_dostream = pc.getValues(anno.chr[g], anno.stop[g], anno.strand[g],
+                                           sense, args.gene_bp, args.downstream_bp)
+        else:
+            values_upstream = pc.getValues(anno.chr[g], anno.stop[g], anno.strand[g],
+                                           sense, args.upstream_bp, args.gene_bp)
+            values_dostream = pc.getValues(anno.chr[g], anno.start[g], anno.strand[g],
+                                           sense, args.gene_bp, args.downstream_bp)
+        if values_upstream is not None and values_dostream is not None:
+            data = np.hstack((values_upstream, values_dostream))
+            smooth_data = pd.Series(data).rolling(window=args.smooth_window, center=True,
+                                                  min_periods=0).mean()
+            return smooth_data
+
+    def write_out_data(sense, out_file, bs_file):
+        data_dict = {}
+        for g in range(anno.size()):
+            data = aggregate_data(g, sense)
+            data_dict[g] = data
+        ids = np.array(list(data_dict.keys()))
+
+        # actual smoothed curve
+        with open(out_file, 'w') as out:
+            avg_vec = np.zeros(cut_len)
+            for ts_ind in ids:
+                avg_vec += data_dict[ts_ind]
+            avg_vec /= len(ids)
+            print(*avg_vec, sep='\t', file=out)
+
+        # bootstrap replicates
+        with open(bs_file, 'w') as out:
+            for bs in range(args.n_bs_iterations):
+                avg_vec = np.zeros(cut_len)
+                ts_ind = np.random.choice(ids, size=len(ids))
+                for ind in ts_ind:
+                    avg_vec += data_dict[ind]
+                avg_vec /= len(ts_ind)
+                print(*avg_vec, sep='\t', file=out)
+
+    prefix_fmt = '%s_centerBoth_up%s_gene%s_do%s_min%s_max%s'
+    fmt_args = (
+        args.prefix,
+        args.upstream_bp,
+        args.gene_bp,
+        args.downstream_bp,
+        args.min_ts_len,
+        args.max_ts_len,
+    )
+    fn_prefix = prefix_fmt % fmt_args
+
+    sense_table = os.path.join(args.outputdir, fn_prefix + '_sense.table')
+    sense_bs_table = os.path.join(args.outputdir, fn_prefix + '_sense_bs.table')
+
+    asense_table = os.path.join(args.outputdir, fn_prefix + '_asense.table')
+    asense_bs_table = os.path.join(args.outputdir, fn_prefix + '_asense_bs.table')
+
+    write_out_data(True, sense_table, sense_bs_table)
+    write_out_data(False, asense_table, asense_bs_table)
+
+    scriptPath = os.path.dirname(os.path.realpath(__file__))
+    plot_script = os.path.join(scriptPath, 'plotCenterBothEnds_bs.R')
+
+    if not args.title:
+        title = args.prefix
+    else:
+        title = args.title
+
+    outfile_pdf = os.path.join(args.outputdir, fn_prefix + '_sm%s.pdf' % args.smooth_window)
+
+    plot_cmd = [
+        'R',
+        '-q',
+        '--slave',
+        '-f %r' % plot_script,
+        '--args',
+        '%r' % sense_table,
+        '%r' % sense_bs_table,
+        '%r' % asense_table,
+        '%r' % asense_bs_table,
+        '%r' % outfile_pdf,
+        '%r' % title,
+        args.upstream_bp,
+        args.downstream_bp,
+        args.gene_bp,
+        args.smooth_window,
+        '%r' % args.labelCenterA,
+        '%r' % args.labelBody,
+        '%r' % args.labelCenterB,
+    ]
+    execute(plot_cmd)
+    if args.cleanup:
+        os.remove(sense_table)
+        os.remove(sense_bs_table)
+        os.remove(asense_table)
+        os.remove(asense_bs_table)
+
+
+if __name__ == '__main__':
+    main()
---
+++ stammp/plots/plotCenterBothEnds_bs.R
@@ -0,0 +1,89 @@
+#!/usr/bin/env Rscript
+library(LSD)
+
+sense = as.matrix(read.table(commandArgs(TRUE)[1],header=F))
+sense_bs = as.matrix(read.table(commandArgs(TRUE)[2],header=F))
+asense = as.matrix(read.table(commandArgs(TRUE)[3],header=F))
+asense_bs = as.matrix(read.table(commandArgs(TRUE)[4],header=F))
+
+outputfile  = commandArgs(TRUE)[5]
+fname       = commandArgs(TRUE)[6]
+rStart      = as.numeric(commandArgs(TRUE)[7])
+rStop       = as.numeric(commandArgs(TRUE)[8])
+body        = as.numeric(commandArgs(TRUE)[9])
+hw          = as.numeric(commandArgs(TRUE)[10])
+centerName1 = commandArgs(TRUE)[11]
+midName     = commandArgs(TRUE)[12]
+centerName2 = commandArgs(TRUE)[13]
+
+
+plotBothEnds = function(sense, sense_bs, asense, asense_bs, relativeSTART, relativeSTOP, bodysize, centerNameA, midName, centerNameB, main=''){
+    lwd  = 3
+
+    bss_quantile <- apply(sense_bs, 2, quantile, probs = c(0.025, 0.975),  na.rm = TRUE)
+    bsa_quantile <- apply(asense_bs, 2, quantile, probs = c(0.025, 0.975),  na.rm = TRUE)
+    gradient_sense   = rev(colorpalette('blues', 100))
+    gradient_asense  = rev(colorpalette('greens', 100))
+    s_col = gradient_sense[95]
+    s_ci_col = paste(s_col, '40', sep='')
+    a_col = gradient_asense[95]
+    a_ci_col = paste(a_col, '40', sep='')
+
+    max_sbs = max(bss_quantile)
+    max_abs = max(bsa_quantile)
+    maxy = max(c(sense, asense, max_sbs, max_abs))
+    shift = 50
+    plot(x=10000, y=10000, xlim=c(1,length(sense)+shift), ylim=c(0,maxy), ylab='Averaged Occupancy', xlab='Genomic position', xaxt='n', main=main)
+        sel = 1:(relativeSTART+bodysize)
+        lines(x=sel, sense[sel],  lwd=lwd, col=s_col)
+        lines(x=sel, asense[sel], lwd=lwd, col=a_col)
+        polygon(c(sel, rev(sel)), c(bss_quantile[1,][sel], rev(bss_quantile[2,][sel])), col=s_ci_col, border=NA)
+        polygon(c(sel, rev(sel)), c(bsa_quantile[1,][sel], rev(bsa_quantile[2,][sel])), col=a_ci_col, border=NA)
+
+        #abline(v=c(relativeSTART, (relativeSTART+bodysize)), lwd=2, lty = 2 , col='black')
+        abline(v=relativeSTART, lwd=1, lty = 1 , col='black')
+        abline(v=relativeSTART+bodysize, lwd=1, lty = 2 , col='darkgrey')
+        abline(v=relativeSTART+bodysize+shift, lwd=1, lty = 2 , col='darkgrey')
+        sel = (relativeSTART+bodysize):length(sense)
+        lines(x=sel+shift, sense[sel],  lwd=lwd, col=s_col)
+        lines(x=sel+shift, asense[sel], lwd=lwd, col=a_col)
+        polygon(c(sel + shift, rev(sel + shift)), c(bss_quantile[1,][sel], rev(bss_quantile[2,][sel])), col=s_ci_col, border=NA)
+        polygon(c(sel + shift, rev(sel + shift)), c(bsa_quantile[1,][sel], rev(bsa_quantile[2,][sel])), col=a_ci_col, border=NA)
+        abline(v=length(sense)+shift-relativeSTOP, lwd=1, lty = 1 , col='black')
+        axis(side=1, at=c(1,round(relativeSTART/2), relativeSTART, relativeSTART+round(bodysize/2), relativeSTART+bodysize+round(shift/2), 
+        relativeSTART+bodysize+shift+round(bodysize/2), length(sense)+shift-relativeSTOP,length(sense)+shift-round(relativeSTOP/2),length(sense)+shift),
+        labels=c(paste('-',relativeSTART), paste('-',round(relativeSTART/2)), centerNameA, paste('+',round(bodysize/2)), 
+        midName, paste('-',round(bodysize/2)), centerNameB, paste('+',round(relativeSTOP/2)), relativeSTOP))
+}
+
+plotAsMat = function(sense, asense, relativeSTART, relativeSTOP, bodysize, centerNameA, midName, centerNameB, main=''){
+    mat = rbind(sense, asense)
+    image(x=1:ncol(mat), y=1:nrow(mat), z=t(mat)[,nrow(mat):1], col=rev(colorpalette('rdbu', 100)), xaxt='n', yaxt='n', ylab='', xlab='Genomic position', main=main)
+    abline(v=relativeSTART+bodysize, lwd=3)
+    axis(side=1, at=c(1,round(relativeSTART/2), relativeSTART, relativeSTART+round(bodysize/2), relativeSTART+bodysize, 
+    relativeSTART+bodysize+round(bodysize/2), length(sense)-relativeSTOP,length(sense)-round(relativeSTOP/2),length(sense)),
+    labels=c(paste('-',relativeSTART), paste('-',round(relativeSTART/2)), centerNameA, paste('+',round(bodysize/2)), 
+    midName, paste('-',round(bodysize/2)), centerNameB, paste('+',round(relativeSTOP/2)), relativeSTOP))
+    axis(side=2, at=c(1,2), labels=c('asense', 'sense'))
+}
+
+pdf(file=outputfile, width=11, height=12.5)
+par(mfcol=c(2,1))
+
+plotBothEnds(sense, sense_bs, asense, asense_bs, rStart, rStop, body, centerName1, midName, centerName2, paste('Signal', fname))
+
+plotAsMat(sense, asense, rStart, rStop, body, centerName1, midName, centerName2, paste('Signal as heatplot', fname))
+dev.off()
+
+
+
+
+
+
+
+
+
+
+
+
+
